5403276356961d8d970ed57d28636a383062767a
FZsiga02 <email@example.com>
2023-03-14 10:48:19 +0100

    Main, Introduction

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,7 +1,20 @@
 import { Component } from "react";
+import profile from '../profile.png';
 
 export default class Introduction extends Component {
     render() {
-        return <p>Introduction</p>
+        return <div>
+            <h1>Történet</h1>
+            <p>A Petrik Webehely-gyárat 2011-ben alapították, hogy az egyre növekvő webes igényeket ki tudja elégíteni.</p>
+            <h1>Jelenlegi tagjaink</h1>
+            <img src={profile} alt="Fotó a főnökről" id="fonok"/>
+            <ul>
+                <li>Erdős Ildikó</li>
+                <li>Jónás Viola</li>
+                <li>Kató Anna</li>
+                <li>Pintér Koppány</li>
+                <li>Sas Gizi</li>
+            </ul>
+        </div>
     }
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,10 @@ import { Component } from "react";
 
 export default class Main extends Component {
     render() {
-        return <p>Main</p>
+        return <div>
+            <p>Üdvözöljük a Petrik Webhely-gyár oldalán!</p>
+            <br />
+            <p>Válasszon a fenti menüpontok közül!</p>
+        </div>
     }
 }
